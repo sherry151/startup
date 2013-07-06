3096213869748dcc99e43f46c2ac040987b8523c
hora89 <hora2rishi@gmail.com>
2013-07-06 19:34:20 +0000

    changes to web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require ('fs');
+buf=fs.readFileSync('index.html');
+var string = buf.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
